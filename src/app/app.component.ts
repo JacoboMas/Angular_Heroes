71a364803a6b7a31a962a07ae9479ac959dbc525
JacoboMas <xacoss@gmail.com>
2024-07-02 14:42:30 +0200

    componente heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from "./heroes/heroes.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HeroesComponent]
 })
 export class AppComponent {
   title = 'angular-tour-of-heroes';
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.scss'
+})
+export class HeroesComponent implements OnInit {
+
+  hero = 'Windstorm';
+
+  constructor(){
+    
+  }
+
+  ngOnInit(){
+    
+  }
+ 
+}
